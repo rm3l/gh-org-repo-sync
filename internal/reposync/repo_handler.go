6932f0683136f3a830b8d54171012bab19e343ea
Armel Soro <armel@rm3l.org>
2022-02-09 23:13:39 +0100

    Don't use an underscore in package names (golint)

--- internal/reposync/repo_handler.go
+++ internal/reposync/repo_handler.go
@@ -1,4 +1,4 @@
-package repo_sync
+package reposync
 
 import (
 	"errors"
--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"github.com/rm3l/gh-org-repo-sync/internal/github"
-	"github.com/rm3l/gh-org-repo-sync/internal/repo_sync"
+	"github.com/rm3l/gh-org-repo-sync/internal/reposync"
 	"log"
 	"os"
 	"strings"
@@ -36,9 +36,9 @@ Example: "language:Go stars:>10 pushed:>2010-11-12"
 See https://bit.ly/3HurHe3 for more details on the search syntax`)
 	flag.IntVar(&batchSize, "batch-size", defaultBatchSize,
 		"the number of elements to retrieve at once. Must not exceed 100")
-	flag.StringVar(&protocol, "protocol", string(repo_sync.SystemProtocol),
-		fmt.Sprintf("the protocol to use for cloning. Possible values: %s, %s, %s.", repo_sync.SystemProtocol,
-			repo_sync.SSHProtocol, repo_sync.HTTPSProtocol))
+	flag.StringVar(&protocol, "protocol", string(reposync.SystemProtocol),
+		fmt.Sprintf("the protocol to use for cloning. Possible values: %s, %s, %s.", reposync.SystemProtocol,
+			reposync.SSHProtocol, reposync.HTTPSProtocol))
 	flag.StringVar(&output, "output", ".", "the output path")
 	flag.Usage = func() {
 		//goland:noinspection GoUnhandledErrorResult
@@ -63,7 +63,7 @@ See https://bit.ly/3HurHe3 for more details on the search syntax`)
 			batchSize)
 		os.Exit(1)
 	}
-	cloneProtocol := repo_sync.CloneProtocol(strings.ToLower(protocol))
+	cloneProtocol := reposync.CloneProtocol(strings.ToLower(protocol))
 
 	repositories, err := github.GetOrganizationRepos(organization, query, batchSize)
 	if err != nil {
@@ -80,7 +80,7 @@ See https://bit.ly/3HurHe3 for more details on the search syntax`)
 	for _, repository := range repositories {
 		go func(repo github.RepositoryInfo) {
 			defer wg.Done()
-			err := repo_sync.HandleRepository(dryRun, output, organization, repo, cloneProtocol)
+			err := reposync.HandleRepository(dryRun, output, organization, repo, cloneProtocol)
 			if err != nil {
 				log.Println("[warn] an error occurred while handling repo", repo, err)
 			}
